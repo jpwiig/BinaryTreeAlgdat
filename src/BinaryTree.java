9256ac7d865a696a524e1b3ed5177489f2771072
jpwiig <jpwiig@gmail.com>
2022-10-20 14:54:24 +0200

    latest from the lecture

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,4 +1,8 @@
+import java.util.ArrayDeque;
+
 public class BinaryTree {
+    Node root;
+
     static class Node {
         char val;
         Node leftChild;
@@ -9,20 +13,54 @@ public class BinaryTree {
             this.leftChild = null;
             this.rightChild = null;
         }
-        void addLeftChild (char val){
-            this.leftChild = new Node(val);
+        Node addLeftChild (char val){
+            return this.leftChild = new Node(val);
         }
-        void addRightChild (char val){
-            this.rightChild = new Node(val);
+        Node addRightChild (char val){
+          return this.rightChild = new Node(val);
         }
 
 
     }
+    BinaryTree(char []val){
+        ArrayDeque<Node> queue = new ArrayDeque<>();
+        root = new Node(val[0]);
+        queue.addLast(root);
 
+        int i = 1;
+        while (i < val.length){
+            Node current = queue.removeFirst();
+            current.addLeftChild(val[i]);
+            queue.addLast(current.leftChild);
+
+            if (i < val.length -1) {
+                current.addRightChild(val[i + 1]);
+                queue.addLast(current.rightChild);
+            }
+            i +=2;
+        }
+    }
+    public static void printDepthFirst (Node current, int order){
+        if (current == null) return;
+       if(order == 1) System.out.print(current.val);
+        printDepthFirst(current.leftChild, order);
+        if (order == 2) System.out.print(current.val);
+        printDepthFirst(current.rightChild, order);
+        if (order == 3) System.out.print(current.val);
+
+    }
     public static void main(String[] args) {
 
-        Node root = new Node('Ø');
+        Node root = new Node('!');
         System.out.println(root.val + "\n " + root.rightChild + "\n " + root.leftChild);
+
+        char [] vals = "!GTALDA".toCharArray();
+        BinaryTree bt = new BinaryTree(vals);
+        printDepthFirst(bt.root, 1);
+        System.out.println();
+        printDepthFirst(bt.root, 2);
+        System.out.println();
+        printDepthFirst(bt.root, 3);
 
     }
 }
