c2c4a59330c12435f0f1cc5bfd0de3376217c02d
jpwiig <jpwiig@gmail.com>
2022-10-20 09:23:55 +0200

    init 2

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class BinaryTree {
+public class BinaryTree {
+    static class Node {
+        char val;
+        Node leftChild;
+        Node rightChild;
+
+        Node(char val){
+            this.val = val;
+            this.leftChild = null;
+            this.rightChild = null;
+        }
+        void addLeftChild (char val){
+            this.leftChild = new Node(val);
+        }
+        void addRightChild (char val){
+            this.rightChild = new Node(val);
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+        Node root = new Node('Ø');
+        System.out.println(root.val + "\n " + root.rightChild + "\n " + root.leftChild);
+
+    }
 }
